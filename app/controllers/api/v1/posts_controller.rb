041785405c05c7fca72dd87e4df1dc5d3a4c9c73
Mohamed Abd El-Meged <mabdelmeged.se@gmail.com>
2023-02-24 17:17:58 +0200

    add post

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::PostsController < ApplicationController
         posts = Post.all
         render json: posts
     end
+    def create
+        post = Post.new(post_params)
+        if post.save!
+            render json: post
+        else
+            render json post.errors
+        end
+    end
+    def post_params
+        params.require(:post).permit(:title,:body)
+    end
 end
